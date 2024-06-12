9e553b358a709d2e95c24461be258ee4da523419
PierreGuede <pierrelarssonkhalifa@gmail.com>
2024-06-12 13:29:13 +0200

    commit

--- src/app/location/[id]/page.tsx
+++ src/app/location/[id]/page.tsx
@@ -1,21 +1,38 @@
+"use client"
 import { NavBar } from "@/components/menu/header/NavBar";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import React from "react";
+import Autoplay from "embla-carousel-autoplay"
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function LocatrionID({ params }: { params: { id: number } }) {
+    const plugin = React.useRef(
+        Autoplay({ delay: 2000, stopOnInteraction: true })
+    )
     return (
         <div>
             <NavBar />
             <div className="w-10/12 mx-auto flex space-x-4">
                 <div className="w-7/12 p-2 bg-blue-200 rounded-md">
-                    <div className="grid  grid-flow-col gap-4 ">
-                        {Array.from({ length: 3 }).map((_, index) => (
-                            <Image key={index} src="/maison.jpg" alt="picture image" className={cn(index == 0 ? ' row-span-2' : 'col-span-2 ', 'bg-blue-400')} width={index == 0 ? 450 : 200} height={index == 0 ? 400 : 200} />
-                        ))}
-                    </div>
+                    <Carousel className="w-full max-w-lg   ">
+                        <CarouselContent className="">
+                            {Array.from({ length: 5 }).map((_, index) => (
+                                <CarouselItem key={index} className="">
+                                    <div className="p-1 flex  items-center justify-center ">
+                                        <Image key={index} src="/maison.jpg" className="rounded-md" alt="picture image" width={550} height={450} />
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious size="icon" className="left-2   bg-transparent" />
+                        <CarouselNext size="icon" className="right-2   bg-transparent" />
+                    </Carousel>
+
                     <div className="py-8 space-y-4">
                         <div>
-                            
+
                         </div>
                     </div>
                 </div>
